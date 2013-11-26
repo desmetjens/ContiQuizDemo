f4d1e69fdb0193b840e878ae930fca7ba2308ee3
Jens Desmet <smetje88@hotmail.com>
2013-11-26 17:40:16 +0100

    Add to github

---
+++ Scripts/App/App.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    var viewModel = new AppViewModel(Quiz);
+    ko.applyBindings(viewModel);
+});
+
+function AppViewModel(data) {
+    var self = this;
+
+    self.Quiz = ko.observable(data);
+    self.CurrentQuestion = ko.observable();
+
+    self.StartQuiz = function () {
+        self.CurrentQuestion(self.Quiz().Questions[0]);
+    };
+
+}
+
